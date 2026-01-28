378520e2ff1ae0bf088f1f78e3950154ef83494b
dituihuagui <48754049+dituihuagui@users.noreply.github.com>
2026-01-28 23:25:43 +0800

    Update models.py

--- openfgl/flcore/FedLMC/models.py
+++ openfgl/flcore/FedLMC/models.py
@@ -9,30 +9,15 @@ class MyModel(nn.Module):
                  hid_dim_node, num_layers_node_encoder=2, num_layers_node_decoder=2,
                  dropout_node=0.5, dropout_cla=0.5, alpha_adj=0.5, adj_fuse=True, topk=10, epsilon=None,
                  num_pers_cos=16, weightedcosine=True, num_border=0,node_border_mask=None):
-        # input_dim：节点原始特征维度
-        # output_dim：输出维度，一般为类别数
-
-        # hid_dim_cla：分类器隐藏层维度
-        # num_layers_cla：分类器层数
-        # dropout_cla：分类器的dropout率
-
-        # num_nodes：客户端原始节点数
-        # embed_dim：随机生成的节点嵌入维度
-        # hid_dim_node：节点特征生成器隐藏层维度
-
-        # num_layers_node：节点特征生成器的网络层数
-        # dropout_node：节点特征生成器的dropout率
 
         super(MyModel, self).__init__()
 
-        # 邻域节点生成器
         self.border_gen = BorderNodeGen(num_nodes=num_border, embed_dim=embed_dim, hid_dim_node=hid_dim_node,
                                         feat_shape=input_dim,
                                         num_layers_node_encoder=num_layers_node_encoder,
                                         num_layers_node_decoder=num_layers_node_decoder,
                                         dropout_node=dropout_node)
 
-        # 分类器
         self.local_compensation = LocMesCom(input_dim=input_dim, topk=topk, epsilon=epsilon, num_pers_cos=num_pers_cos,
                                             weightedcosine=weightedcosine, adj_fuse=adj_fuse, alpha_adj=alpha_adj,
                                             hid_dim_cla=hid_dim_cla, output_dim=output_dim, num_layers_cla=num_layers_cla,
@@ -71,7 +56,6 @@ class LocMesCom(nn.Module):
         self.classifier.reset_parameters()
 
     def segment_compensate(self, node_feature, edge_index=None, border_node=None, border_subgraph_map=None):
-        # 将第一阶段新增的语义邻居加入到原始图结构中，共同完成消息传播
         adj_ori = self.edge_gen(node_feature)
         adj_normalize = adj_ori / torch.clamp(torch.sum(adj_ori, dim=-1, keepdim=True),max=1, min=1e-12)  # 归一化
         #adj_normalize=symmetry(adj_normalize)
@@ -84,7 +68,6 @@ class LocMesCom(nn.Module):
         return adj_normalize, adj_normalize
 
     def original_graph(self, node_feature, edge_index=None, border_node=None, border_subgraph_map=None):
-        # 将第一阶段新增的语义邻居加入到原始图结构中，共同完成消息传播
         num_nodes = node_feature.shape[0]
         adj_ini = torch.sparse_coo_tensor(edge_index, torch.ones(edge_index.size(1)).to(node_feature.device),
                                           torch.Size([num_nodes, num_nodes])).to_dense()
@@ -94,7 +77,6 @@ class LocMesCom(nn.Module):
     def cal_border_weight(self, node_feature, border_nodes):
         weight_border = self.edge_gen.metric(node_feature, border_nodes)
         weight_border=torch.clamp(weight_border, max=1, min=1e-12)
-        '''weight_border = weight_border / torch.clamp(torch.sum(weight_border, dim=-2, keepdim=True), max=1, min=1e-12)  # 归一化'''
         weight_border = weight_border * self.node_border_mask + 0 * (1 - self.node_border_mask)
         return weight_border
 
@@ -106,7 +88,6 @@ class LocMesCom(nn.Module):
             embedding, logits = self.classifier(x, adj, border_nodes, weight_border)
         else:
             embedding, logits = self.classifier(x, adj)
-        # self.adj_learn = adj_learn
         self.border_embedding = self.classifier.border_embedding
         return embedding, logits
 
@@ -115,7 +96,6 @@ class BorderNodeGen(nn.Module):
     def __init__(self, num_nodes, embed_dim, hid_dim_node, feat_shape, num_layers_node_encoder, num_layers_node_decoder,
                  dropout_node):
         super(BorderNodeGen, self).__init__()
-        # 生成初始节点随机嵌入部分
         self.num_nodes = num_nodes
         self.embed_dim = embed_dim
         self.ini_emb = nn.Parameter(torch.zeros(self.num_nodes, self.embed_dim))  # 随机生成的节点embedding
@@ -127,7 +107,6 @@ class BorderNodeGen(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        #nn.init.xavier_uniform_(self.ini_emb)
         self.node_gen.reset_parameters()
 
     def learn_node(self, node_embedding):
